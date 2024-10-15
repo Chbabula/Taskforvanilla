141dd9741935d7714d86237b6fb33d60c967648b
Chbabula <mai2babulal786@gmail.com>
2024-10-15 11:21:30 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Number from './components/Number';
 
 function App() {
+  const numbersArray = [1, 2, 3, 4, 5];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Number Cycler</h1>
+      <Number numbers={numbersArray} />
     </div>
   );
 }
